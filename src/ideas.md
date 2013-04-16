ff0a516d12db8d76ca315c2c43a946de8fd6b27a
Nicholas Ren <nicholas85211@gmail.com>
2013-04-17 00:27:41 +0800

    add tests for request dispatching

--- src/ideas.md
+++ src/ideas.md
@@ -1,7 +1,2 @@
-+ interface `ServletContext` was supposed to defining a view of the Web application
-
-+ Programmatically adding and configuring Servlets
-
-+ get resources though `ServletContext.getResource`
-
-+ annotation in classes under `WEB-INF/classes` will be processed.
+1. how to initiate a controller instance?
+   marked @controller?
--- src/test/java/com/thoughtworks/mvc/core/DispatchServletTest.java
+++ src/test/java/com/thoughtworks/mvc/core/DispatchServletTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
@@ -27,22 +29,36 @@ public class DispatchServletTest {
     @Before
     public void setUp() {
         servlet = new DispatchServlet();
+        ServletConfig config = mock(ServletConfig.class);
+        when(config.getInitParameter("module-name")).thenReturn("com.example");
+        servlet.init(config);
     }
 
     @Test
     public void should_create_a_injector_after_initialized() throws ServletException {
-        ServletConfig config = mock(ServletConfig.class);
-        servlet.init(config);
         assertThat(servlet.getContainer(), notNullValue());
     }
 
     @Test
     public void should_get_route_info_on_initialization() throws ServletException, IOException {
-        ServletConfig config = mock(ServletConfig.class);
-        when(config.getInitParameter("module-name")).thenReturn("com.example");
-        servlet.init(config);
-
         Router router = servlet.getRouter();
         assertThat(router.classFor("/user"), equalTo(UserController.class));
+    }
+
+    @Test
+    public void should_create_controller_from_injector(){
+
+    }
+
+    //RESTful url mapping
+    @Test
+    public void should_dispatch_get_by_id_request_to_show_action() throws ServletException, IOException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getRequestURI()).thenReturn("/user/1");
+        when(request.getMethod()).thenReturn("GET");
+
+        servlet.service(request, response);
+
     }
 }
