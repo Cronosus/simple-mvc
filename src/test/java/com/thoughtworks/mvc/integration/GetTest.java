3220b445f9735abaf1cad263f0ba5b7f511cd5a5
Nicholas Ren <nicholas85211@gmail.com>
2013-04-16 12:57:49 +0800

    add integration test

---
+++ src/test/java/com/thoughtworks/mvc/integration/GetTest.java
@@ -0,0 +1,37 @@
+package com.thoughtworks.mvc.integration;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class GetTest {
+
+    public static final String JETTY_SERVER_URL = "http://localhost:8080/sample/";
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void should_response_get_by_id_request() {
+        driver.get(JETTY_SERVER_URL + "user/1");
+        assertThat(getBody(), is("this is user with id 1"));
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    private String getBody() {
+        return driver.findElement(By.tagName("body")).getText();
+    }
+}
